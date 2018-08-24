d62be1b8268e7f0b3b08f9291980852b0603a804
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2018-08-24 10:35:39 -0400

    Updated Graduated Students Without Scheduled Test Date

--- Query Reports/Graduated Students Without Scheduled Test Date.sql
+++ Query Reports/Graduated Students Without Scheduled Test Date.sql
@@ -2,29 +2,47 @@
 -- Author: Kelly MJ   |   Creation date: 8/3/2018
 -- Purpose: Display a list of students who have graduated since the "Report Start Date" who have not scheduled their state board exam.
 		 -- The list is ordered by the least-to-most recent graduation dates
+-- 8/24/18 update:
+    -- Corrected Pending Grad status code from 16 to 17
+    -- Restructured joins
+    -- Now uses graduationDate instead of endDate to determine student grad dates
 
 SELECT S.idNumber 'Student ID'
 	 , CONCAT('<a href="admin_view_student.jsp?studentid=', CAST(S.studentId AS CHAR), '">', S.firstName, ' ', S.lastName, '</a>') AS 'Student Name'
-     , MAX(R.endDate) AS 'Graduation Date'
+     , DATE_FORMAT(R.graduationDate, '%m/%d/%Y') AS 'Graduation Date'
      , CASE WHEN R.regStatus =  3 THEN 'Graduated'
-            WHEN R.regStatus = 16 THEN 'Pending Grad'
+            WHEN R.regStatus = 17 THEN 'Pending Grad'
 	   END AS 'Status'
+     -- , PFV.fieldValue AS 'State Board Exam Date'
 
 FROM Registrations R
-   , Students S
-   , Programmes P
-   , ProfileFieldValues PFV
 
-WHERE R.studentId = S.studentId			-- Student table join
-  AND R.programmeId = P.programmeId		-- Programme table join
-  AND P.programmeName NOT LIKE '%career%'
-  AND R.studentId = PFV.userId			-- PFV table join
-  AND PFV.fieldName LIKE 'EXAM_DATE'
-  AND (PFV.fieldValue IS NULL OR PFV.fieldValue < '1970-01-01')
-  AND R.regStatus IN (3, 16)			-- Registration/graduation criteria
+INNER JOIN Students S
+    ON S.studentId = R.studentId
+
+INNER JOIN Programmes P
+    ON P.programmeId = R.programmeId
+    AND P.programmeName NOT LIKE '%career%'
+
+INNER JOIN ProfileFieldValues PFV
+    ON PFV.userId = R.studentId
+    AND PFV.fieldName LIKE 'EXAM_DATE'
+    AND (PFV.fieldValue IS NULL OR PFV.fieldValue < '1970-01-01')
+
+INNER JOIN (
+    SELECT studentId
+        , MAX(endDate) AS maxEnd
+        , registrationDate
+    FROM Registrations 
+    GROUP BY studentId) RR
+    ON RR.studentId = R.studentId
+    AND RR.maxEnd = R.endDate
+
+WHERE
+  R.regStatus IN (3, 17)			-- Grad or Pending Grad status
   AND R.isActive = 1
-  AND R.enrollmentSemesterId = 4000441
-  AND R.endDate > '[?Report Start Date]'
+  -- AND R.enrollmentSemesterId = 4000441
+  AND R.registrationDate > '[?Report Start Date]'
   AND R.<ADMINID>
 GROUP BY S.studentId
 
