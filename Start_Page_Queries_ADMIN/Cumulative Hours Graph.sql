186800069f36f83e876121344dfff62465ed34af
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2018-09-05 09:56:40 -0400

    Changed Cumulative Hours Graph

--- Start_Page_Queries_ADMIN/Cumulative Hours Graph.sql
+++ Start_Page_Queries_ADMIN/Cumulative Hours Graph.sql
@@ -2,12 +2,20 @@
 -- Author: Kelly MJ  |  08/29/2018
 -- Shows cumulative hours attended per day for the past 7 days
 
-SELECT A.attendanceDate
-	, SUM(A.duration) AS 'Total Attendance Hours vs. Date'
-    
-FROM Attendance A
+SELECT t1.dateAtt AS 'Date'
+	, t1.total AS 'cumulative hours'
 
-WHERE A.attendanceDate >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) AND A.attendanceDate < CURDATE()
-AND A.<ADMINID>
+FROM (
+	SELECT DATE_FORMAT(A.attendanceDate, '%d') AS dateAtt
+	    , DAYOFWEEK(A.attendanceDate) AS day
+		, SUM(A.duration) AS total
+	    
+	FROM Attendance A
 
-GROUP BY A.attendanceDate
+	WHERE A.attendanceDate >= DATE_SUB(CURDATE(), INTERVAL 31 DAY) AND A.attendanceDate < CURDATE()
+	AND A.<ADMINID>
+
+	GROUP BY A.attendanceDate
+) t1
+
+WHERE t1.total > 0
