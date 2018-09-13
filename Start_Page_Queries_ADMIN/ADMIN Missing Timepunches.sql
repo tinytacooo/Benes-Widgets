199e7b0ac2cf5f19bd0fc4162fe207b768865121
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2018-09-13 08:24:07 -0400

    Missing Timepunches

---
+++ Start_Page_Queries_ADMIN/ADMIN Missing Timepunches.sql
@@ -0,0 +1,18 @@
+-- Kelly MJ 8/24/2018 update: use attendanceClockPunch instead of reasonText
+
+SELECT
+	CONCAT('<a href="view_attendance.jsp?semesterid=', CAST(REG.enrollmentSemesterId AS CHAR), '&classid=', CAST(ATD.classId AS CHAR), '&subjectid=', CAST(ATD.subjectId AS CHAR), '&studentid=', CAST(ATD.studentId AS CHAR), '"target="_blank">', CAST(SDT.firstName AS CHAR), ' ', CAST(SDT.lastName AS CHAR), '</a>') AS Name,
+	ATD.duration AS Duration,
+	ATD.attendanceDate AS Attendance_Date,
+        ATD.attendanceClockPunch
+FROM
+	Attendance ATD
+	INNER JOIN Registrations REG ON ATD.studentId = REG.studentId
+	INNER JOIN Students SDT ON ATD.studentId = SDT.studentId
+WHERE
+	ATD.<ADMINID> AND NOT ATD.isActive = 0 AND
+	YEAR(ATD.attendanceDate) > 2017 AND
+	ATD.duration = 0 AND ATD.present = 1
+GROUP BY Attendance_Date, Name
+ORDER BY SDT.firstName, Attendance_Date
+LIMIT 10000
