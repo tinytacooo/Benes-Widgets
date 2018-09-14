7ee55f01e4b13bcc9d0a33e166411fb33940e65f
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2018-09-14 08:34:22 -0400

    Added CDIR VA by Campus

--- Start_Page_Queries_ADMIN/ADMIN Lead Type Counts.sql
+++ Start_Page_Queries_ADMIN/ADMIN Lead Type Counts.sql
@@ -1,21 +1,80 @@
 -- ADMIN Widget: Lead Type Counts
--- Kelly MJ  |  09/10/2018
+-- Kelly MJ  |  09/10/2018 WITH HEADERS
 
--- 1. New Leads
-SELECT SA.campusCode
-	, CT.typeName AS Type
-	, COUNT(C.contactId) AS Count
+-- New Port Richey
+SELECT NULL AS 'Lead Type'
+	, '<tr style="text-align: left; background-color: #ADD8E6;"><td style="font-size: 125%; font-weight: bold;">New Port Richey</td><td></td></tr>' AS 'Count'
 
-FROM Contacts C
+UNION
+SELECT t1.Type, t1.Count
+FROM (
+	SELECT SA.campusCode AS Campus
+		, CT.typeName AS Type
+		, COUNT(C.contactId) AS Count
 
-INNER JOIN ContactTypes CT
-	ON CT.contactTypeId = C.contactTypeId
-	AND CT.contactTypeId IN (4000040, 4000043, 4000044, 4000051, 4000045, 4000042, 4000048, 4000047, 4000050, 4000049)
+	FROM Contacts C
 
-LEFT JOIN SubAdmins SA
-	ON SA.subAdminId = CT.subAdminId
+	INNER JOIN ContactTypes CT
+		ON CT.contactTypeId = C.contactTypeId
+		AND CT.contactTypeId IN (4000040, 4000043, 4000044, 4000051, 4000045, 4000042, 4000048, 4000047, 4000050, 4000049)
 
-WHERE C.isActive = 1
-	AND C.<ADMINID>
+	LEFT JOIN SubAdmins SA
+		ON SA.subAdminId = C.subAdminId
 
-GROUP BY SA.campusCode, CT.typeName
+	WHERE C.isActive = 1
+		AND C.<ADMINID>
+		AND C.subAdminId NOT IN (SELECT subAdminId FROM SubAdmins WHERE campusCode IN (34601, 34606))
+
+	GROUP BY CT.typeName) t1
+
+UNION	-- Spring Hill
+SELECT NULL AS 'Lead Type'
+	, '<tr style="text-align: left; background-color: #ADD8E6;"><td style="font-size: 125%; font-weight: bold;">Spring Hill</td><td></td></tr>' AS 'Count'
+
+UNION 
+SELECT t2.Type, t2.Count
+FROM (
+	SELECT SA.campusCode AS Campus
+		, CT.typeName AS Type
+		, COUNT(C.contactId) AS Count
+
+	FROM Contacts C
+
+	INNER JOIN ContactTypes CT
+		ON CT.contactTypeId = C.contactTypeId
+		AND CT.contactTypeId IN (4000040, 4000043, 4000044, 4000051, 4000045, 4000042, 4000048, 4000047, 4000050, 4000049)
+
+	LEFT JOIN SubAdmins SA
+		ON SA.subAdminId = C.subAdminId
+
+	WHERE C.isActive = 1
+		AND C.<ADMINID>
+
+	GROUP BY Campus, CT.typeName) t2
+WHERE t2.Campus = 34601
+
+UNION	-- Brookesville
+SELECT NULL AS 'Lead Type'
+	, '<tr style="text-align: left; background-color: #ADD8E6;"><td style="font-size: 125%; font-weight: bold;">Brookesville</td><td></td></tr>' AS 'Count'
+
+UNION
+SELECT t3.Type, t3.Count
+FROM (
+	SELECT SA.campusCode AS Campus
+		, CT.typeName AS Type
+		, COUNT(C.contactId) AS Count
+
+	FROM Contacts C
+
+	INNER JOIN ContactTypes CT
+		ON CT.contactTypeId = C.contactTypeId
+		AND CT.contactTypeId IN (4000040, 4000043, 4000044, 4000051, 4000045, 4000042, 4000048, 4000047, 4000050, 4000049)
+
+	LEFT JOIN SubAdmins SA
+		ON SA.subAdminId = C.subAdminId
+
+	WHERE C.isActive = 1
+		AND C.<ADMINID>
+
+	GROUP BY Campus, CT.typeName) t3
+WHERE t3.Campus = 34606
--- Start_Page_Queries_ADMIN/ADMIN New Leads Within the Last Week.sql
+++ Start_Page_Queries_ADMIN/ADMIN New Leads Within the Last Week.sql
@@ -1,20 +1,111 @@
 -- ADMIN Widget: New Leads in the Past Week
 -- Kelly MJ  |  09/10/2018
 
-SELECT CONCAT('<a target="_blank" href="https://benes.orbund.com/einstein-freshair/admin_view_contact.jsp?contactid=', CAST(C.contactId AS CHAR), '"">', C.firstName, ' ', C.lastName, '</a>') AS 'Contact Name'
-	, CT.typeName AS Stage
-	, PFV.fieldValue AS 'Program of Interest'
-	, DATE(C.lastUpdateDtTm) AS 'Last Updated'
+-- New Port Richey
+SELECT NULL 'Contact Name', NULL 'Stage', NULL 'Program of Interest'
+	, '<tr style="text-align: left; background-color: #ADD8E6;"><td style="font-size: 125%; font-weight: bold;">New Port Richey</td><td></td><td></td><td></td></tr>' AS 'Last Updated'
 
-FROM Contacts C
+UNION
+SELECT t1.name 'Contact Name'
+	, t1.type 'Stage'
+	, t1.program 'Program of Interest'
+	, t1.lastUpdate 'Last Updated'
+FROM (
+	SELECT SA.campusCode AS campus
+		, CT.typeName AS type
+		, PFV.fieldValue AS program
+		, CONCAT('<a target="_blank" href="https://benes.orbund.com/einstein-freshair/admin_view_contact.jsp?contactid=', CAST(C.contactId AS CHAR), '"">', C.firstName, ' ', C.lastName, '</a>') AS name
+		, DATE(C.lastUpdateDtTm) AS lastUpdate
 
-LEFT JOIN ProfileFieldValues PFV
-	ON PFV.userId = C.contactId
-	AND PFV.fieldName = 'PROGRAM_OF_INTEREST'
+	FROM Contacts C
 
-LEFT JOIN ContactTypes CT
-	ON CT.contactTypeId = C.contactTypeId
+	INNER JOIN ContactTypes CT
+		ON CT.contactTypeId = C.contactTypeId
+		AND CT.contactTypeId IN (4000040, 4000043, 4000044, 4000051, 4000045, 4000042, 4000048, 4000047, 4000050, 4000049)
 
-WHERE C.contactTypeId IN (4000040, 4000043, 4000044, 4000051, 4000045, 4000042, 4000048, 4000047, 4000049)
-	AND DATE(C.creationDtTm) >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK)
-	AND C.<ADMINID>
+	LEFT JOIN ProfileFieldValues PFV
+		ON PFV.userId = C.contactId
+		AND PFV.fieldName = 'PROGRAM_OF_INTEREST'
+
+	LEFT JOIN SubAdmins SA
+		ON SA.subAdminId = C.subAdminId
+
+	WHERE C.isActive = 1
+		AND C.<ADMINID>
+		AND DATE(C.creationDtTm) >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK)
+		AND C.subAdminId NOT IN (SELECT subAdminId FROM SubAdmins WHERE campusCode IN (34601, 34606))
+
+	GROUP BY CT.typeName) t1
+
+UNION	-- Spring Hill
+SELECT NULL 'Contact Name', NULL 'Stage', NULL 'Program of Interest'
+	, '<tr style="text-align: left; background-color: #ADD8E6;"><td style="font-size: 125%; font-weight: bold;">Spring Hill</td><td></td><td></td><td></td></tr>' AS 'Last Updated'
+
+UNION
+SELECT t2.name 'Contact Name'
+	, t2.type 'Stage'
+	, t2.program 'Program of Interest'
+	, t2.lastUpdate 'Last Updated'
+FROM (
+	SELECT SA.campusCode AS campus
+		, CT.typeName AS type
+		, PFV.fieldValue AS program
+		, CONCAT('<a target="_blank" href="https://benes.orbund.com/einstein-freshair/admin_view_contact.jsp?contactid=', CAST(C.contactId AS CHAR), '"">', C.firstName, ' ', C.lastName, '</a>') AS name
+		, DATE(C.lastUpdateDtTm) AS lastUpdate
+
+	FROM Contacts C
+
+	INNER JOIN ContactTypes CT
+		ON CT.contactTypeId = C.contactTypeId
+		AND CT.contactTypeId IN (4000040, 4000043, 4000044, 4000051, 4000045, 4000042, 4000048, 4000047, 4000050, 4000049)
+
+	LEFT JOIN ProfileFieldValues PFV
+		ON PFV.userId = C.contactId
+		AND PFV.fieldName = 'PROGRAM_OF_INTEREST'
+
+	LEFT JOIN SubAdmins SA
+		ON SA.subAdminId = C.subAdminId
+
+	WHERE C.isActive = 1
+		AND C.<ADMINID>
+		AND DATE(C.creationDtTm) >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK)
+		AND C.subAdminId IN (SELECT subAdminId FROM SubAdmins WHERE campusCode = 34601)
+
+	GROUP BY CT.typeName) t2
+WHERE t2.Campus = 34601
+
+UNION	-- Brookesville
+SELECT NULL 'Contact Name', NULL 'Stage', NULL 'Program of Interest'
+	, '<tr style="text-align: left; background-color: #ADD8E6;"><td style="font-size: 125%; font-weight: bold;">Brookesville</td><td></td><td></td><td></td></tr>' AS 'Last Updated'
+
+UNION
+SELECT t1.name 'Contact Name'
+	, t1.type 'Stage'
+	, t1.program 'Program of Interest'
+	, t1.lastUpdate 'Last Updated'
+FROM (
+	SELECT SA.campusCode AS campus
+		, CT.typeName AS type
+		, PFV.fieldValue AS program
+		, CONCAT('<a target="_blank" href="https://benes.orbund.com/einstein-freshair/admin_view_contact.jsp?contactid=', CAST(C.contactId AS CHAR), '"">', C.firstName, ' ', C.lastName, '</a>') AS name
+		, DATE(C.lastUpdateDtTm) AS lastUpdate
+
+	FROM Contacts C
+
+	INNER JOIN ContactTypes CT
+		ON CT.contactTypeId = C.contactTypeId
+		AND CT.contactTypeId IN (4000040, 4000043, 4000044, 4000051, 4000045, 4000042, 4000048, 4000047, 4000050, 4000049)
+
+	LEFT JOIN ProfileFieldValues PFV
+		ON PFV.userId = C.contactId
+		AND PFV.fieldName = 'PROGRAM_OF_INTEREST'
+
+	LEFT JOIN SubAdmins SA
+		ON SA.subAdminId = C.subAdminId
+
+	WHERE C.isActive = 1
+		AND C.<ADMINID>
+		AND DATE(C.creationDtTm) >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK)
+		AND C.subAdminId IN (SELECT subAdminId FROM SubAdmins WHERE campusCode = 34606)
+
+	GROUP BY CT.typeName) t1
